90ac5f7d26b1b6ab4f89e0a9ea488baa6233e49b
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-05-21 13:06:05 +0530

    Update assistantcall.py

--- YukkiMusic/plugins/admins/assistantcall.py
+++ YukkiMusic/plugins/admins/assistantcall.py
@@ -23,11 +23,10 @@ ASST_COMMAND = get_command("ASST_COMMAND")
 @AdminRightsCheck
 async def join_assistant(self, original_chat_id, chat_id):
     chat = await app.get_chat(chat_id)
-    if chat.username:
-      try:
+    if chat.username:      
         await userbot.join_chat(chat.username)
-         try:
-             invitelink = chat.invite_link
+    else:
+        invitelink = chat.invite_link
         if invitelink is None:
             invitelink = (await app.export_chat_invite_link(chat_id))
         else:
@@ -43,7 +42,7 @@ async def join_assistant(self, original_chat_id, chat_id):
         await userbot.join_chat(invitelink)
         await remove_active_chat(chat_id)
         return await user.send_message(chat_id, "✅ userbot joined this chat")
-      except UserAlreadyParticipant:
+        await userbot.join_chat(invitelink)
         return await user.send_message(chat_id, "✅ userbot already in this chat")
         await asyncio.sleep(4)
         await m.edit(_["call_6"].format(userbot.name))
