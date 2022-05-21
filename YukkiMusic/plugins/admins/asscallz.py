35fa0b433d39505f13b4b1983bbdbaef5c118c36
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-05-21 13:44:25 +0530

    Create asscallz.py

---
+++ YukkiMusic/plugins/admins/asscallz.py
@@ -0,0 +1,24 @@
+async def join_chat(self, original_chat_id, chat_id):
+  language = await get_lang(original_chat_id)
+        _ = get_string(language)
+        userbot = await get_assistant(chat_id)
+    get = await app.get_chat_member(chat_id, userbot.id)
+    chat = await app.get_chat(chat_id)
+    if chat.username:
+    await userbot.join_chat(chat.username)
+    invitelink = chat.invite_link
+    await userbot.join_chat(chat.username)
+    try:
+        invitelink = chat.invite_link
+        if not invitelink:
+            invitelink = (await app.export_chat_invite_link(chat_id))
+            await userbot.join_chat(invitelink)
+        if invitelink.startswith("https://t.me/+"):
+            invitelink = invitelink.replace(
+                "https://t.me/+", "https://t.me/joinchat/"
+            )
+        await userbot.join_chat(invitelink)
+        await remove_active_chat(chat_id)
+        return await userbot.send_message(chat_id, "✅ userbot joined this chat")
+    except UserAlreadyParticipant:
+        return await user.send_message(chat_id, "✅ userbot already in this chat")
