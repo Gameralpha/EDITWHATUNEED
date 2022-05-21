2761c50be40bf25a41fc753ae638bd83d7814d12
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-05-21 12:47:16 +0530

    Update assistantcall.py

--- YukkiMusic/plugins/admins/assistantcall.py
+++ YukkiMusic/plugins/admins/assistantcall.py
@@ -27,33 +27,45 @@ from YukkiMusic.utils.database import (add_active_chat,
                                        remove_active_chat,
                                        remove_active_video_chat,
                                        set_loop)
-from YukkiMusic.utils.exceptions import AssistantErr
 
 
 # Commands
-LOOP_COMMAND = get_command("LOOP_COMMAND")
+ASST_COMMAND = get_command("ASST_COMMAND")
 
 
 @app.on_message(
-    filters.command(LOOP_COMMAND)
+    filters.command(ASST_COMMAND)
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
 )
 @AdminRightsCheck
-async def join_chat(c: Client, m: Message):
-    chat_id = m.chat.id
+async def join_assistant(self, original_chat_id, chat_id):
+    chat = await app.get_chat(chat_id)
+    if chat.username:
     try:
-        invitelink = (await c.get_chat(chat_id)).invite_link
-        if not invitelink:
-            await c.export_chat_invite_link(chat_id)
-            invitelink = (await c.get_chat(chat_id)).invite_link
+        await userbot.join_chat(chat.username)
+    else:
+         try:
+             invitelink = chat.invite_link
+        if invitelink is None:
+            invitelink = (await app.export_chat_invite_link(chat_id))
+        else:
+          invitelink = (
+                                await app.export_chat_invite_link(
+                                    chat_id
+                                )
+                        )
         if invitelink.startswith("https://t.me/+"):
             invitelink = invitelink.replace(
                 "https://t.me/+", "https://t.me/joinchat/"
             )
-        await user.join_chat(invitelink)
+        await userbot.join_chat(invitelink)
         await remove_active_chat(chat_id)
         return await user.send_message(chat_id, "✅ userbot joined this chat")
     except UserAlreadyParticipant:
         return await user.send_message(chat_id, "✅ userbot already in this chat")
+        await asyncio.sleep(4)
+        await m.edit(_["call_6"].format(userbot.name))
+        await userbot.join_chat(invitelink)
+        
