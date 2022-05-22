2ba93f27a2e9be2776346c7169c457490eeb51d1
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-05-22 10:46:32 +0530

    Update asscallz.py

--- YukkiMusic/plugins/admins/asscallz.py
+++ YukkiMusic/plugins/admins/asscallz.py
@@ -26,6 +26,9 @@ async def join_chat(self, original_chat_id, chat_id):
   userbot = await get_assistant(chat_id)
   get = await app.get_chat_member(chat_id, userbot.id)
   chat = await app.get_chat(chat_id)
+  invitelink = await app.export_chat_invite_link(
+                        message.chat.id
+                    )
   if chat.username:
     await userbot.join_chat(chat.username)
     invitelink = chat.invite_link
@@ -43,3 +46,32 @@ async def join_chat(self, original_chat_id, chat_id):
             await remove_active_chat(chat_id)
             return await userbot.send_message(chat_id, "✅ userbot joined this chat")
           
+if message.chat.username:
+                try:
+                    await userbot.join_chat(message.chat.username)
+                except UserAlreadyParticipant:
+                    pass
+                except Exception as e:
+                    await message.reply_text(
+                        f"__Assistant Failed To Join__\n\n**Reason**:peer invalid kindly add manuvally"
+                    )
+                    return
+            else:
+                try:
+                    invitelink = await app.export_chat_invite_link(
+                        message.chat.id
+                    )
+                    if invitelink.startswith("https://t.me/+"):
+                        invitelink = invitelink.replace(
+                            "https://t.me/+", "https://t.me/joinchat/"
+                        )
+                    await userbot.join_chat(invitelink)
+                    await message.reply(
+                        f"userbot Joined Successfully",
+                    )
+                except UserAlreadyParticipant:
+                    pass
+                except Exception as e:
+                    await message.reply_text(
+                        f"__Assista Faile**Reason**:{e} "
+                    )
